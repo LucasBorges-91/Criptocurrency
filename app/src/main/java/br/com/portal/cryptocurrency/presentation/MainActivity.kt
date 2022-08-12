0cc3134ed2ffb384edd9154ee74d78eb0b92f3cd
Lucas Portal <lucas.pborges@yahoo.com.br>
2022-08-12 14:23:35 -0300

    finishin the coin detail screen and end the project.

--- app/src/main/java/br/com/portal/cryptocurrency/presentation/MainActivity.kt
+++ app/src/main/java/br/com/portal/cryptocurrency/presentation/MainActivity.kt
@@ -10,8 +10,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.portal.cryptocurrency.presentation.coin_detail.CoinDetailScreen
+import br.com.portal.cryptocurrency.presentation.coin_list.CoinListScreen
 import br.com.portal.cryptocurrency.presentation.ui.theme.CryptocurrencyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +28,22 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen( navController )
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
